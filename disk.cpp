31ab411aab3efa41f9ef323e108dbfb93d2855c4
tfursten <tfursten@asu.edu>
2014-12-30 15:24:36 -0700

    add comments

--- disk.cpp
+++ disk.cpp
@@ -18,11 +18,13 @@ void Disk::initialize(double r)
 
 double Disk::integrate(double x1, double x2)
 {
+    //determine area of portion of the quarter disk in a column of lattice cells.
     return (0.5 * sqrt(rSq-x2*x2) * x2 + rSq*0.5* asin(x2/radius)) - (0.5 * sqrt(rSq-x1*x1) * x1 + rSq*0.5* asin(x1/radius));
 }
 
 void Disk::getCellRange()
 {
+    //define borders of each lattice cell.
     cellRange.push_back(0.0);
     for(int i=0; i < radius; i++)
     {
@@ -34,6 +36,9 @@ void Disk::getCellRange()
 }
 
 double Disk::getMaxX(){
+    //Find column of cells that intesect the quarter circle at 45 degrees. 
+    //Values for cells in any column after this point can be copied from 
+    //cells in previous columns in mirrored locations.  
     double mX = pol2xy(M_PI/4.0).first;
     for (int i=vecDim; i>=1; i--){
         if((mX <= cellRange[i]) && (mX > cellRange[i-1]))
@@ -43,6 +48,7 @@ double Disk::getMaxX(){
 
 pair<double,double> Disk::pol2xy(double theta)
 {
+    //convert from polar to xy coordinates
     double x = radius*cos(theta);
     double y = radius*sin(theta);
     return make_pair(x,y);
@@ -50,6 +56,7 @@ pair<double,double> Disk::pol2xy(double theta)
 
 double Disk::circle(double &x)
 {
+    //given the radius and x coord. solve for y coord. of circle
     return (double)sqrt(radius*radius-x*x);
 
 }
@@ -62,9 +69,9 @@ int Disk::getBin(double x){
 }
 
 void Disk::areas(double x1, double x2, int i){
+    //find area for each cell in a column starting at the base
+    //each full cell is subtracted from the total area.
     double A = integrate(x1,x2);
-    cout << "X1,X2: " << x1 << " " << x2 << endl;
-    cout << "A: " << A << endl;
     for (int y = 0; y<vecDim; y++){
         double y1 = cellRange[y];
         double y2 = cellRange[y+1];
@@ -85,19 +92,10 @@ void Disk::getAreas(int i){
     double x2 = cellRange[i+1];
     double fx1 = circle(x1);
     double fx2 = circle(x2);
-    cout <<"Start:"<< x1 << " " << x2 << endl;
     if(getBin(fx1) != getBin(fx2)){
-        cout << "INSIDE" << endl;
-        cout << "bin1: " << getBin(fx1) << endl;
-        cout << "bin2: " << getBin(fx2) << endl;
-        cout << "vecDim: " << vecDim << endl;
-        cout << cellRange[getBin(fx1)] << endl;
-        cout << "CellRange: " ;
-        for(int i=0; i<=vecDim; i++)
-            cout << cellRange[i] << " ";
-        cout << endl;
+    //if the arch of the circle crosses through the bottom of a cell, the area 
+    //of that column will be calculated as two separate column fragments.  
         x2 = circle(cellRange[getBin(fx1)]);
-        cout << "X2 AFter: " << x2 << endl;
         areas(x1,x2,i);
         x1 = x2;
         x2 = cellRange[i+1];
@@ -111,8 +109,15 @@ void Disk::getAreas(int i){
 
 
 void Disk::makeTables(){
+    //Calculates the area of lattice cells that are covered by a disk centered
+    //on a focal lattice cell. The areas are then converted into a probability.  
+    //This is simplified by only calculating values for a quarter slice of
+    //disk and mirroring values to the other quadrants.  It is further 
+    //simplified by only calculating approximately half of the values for 
+    //the quarter slice and mirroring those as well.
     for(int i=0; i<maxX; i++)
         getAreas(i);
+     //mirror values to the other half of the quarter disk
     for(int x=maxX; x<vecDim; x++){
         for(int y=0; y<vecDim; y++){
             if (probMap.count(make_pair(y,x)))
@@ -122,7 +127,9 @@ void Disk::makeTables(){
     for(int x=0; x<vecDim; x++){
         for(int y=0; y<vecDim; y++){
             if (probMap.count(make_pair(x,y))){
+                 //convert area to a probability
                 double prob = probMap[make_pair(x,y)]/totalArea;
+                //mirror probability to other 3/4 of the disk
                 probMap[make_pair(x,y)] = prob;
                 probMap[make_pair(x*-1,y*-1)] += prob;
                 probMap[make_pair(x*-1,y)] += prob;
